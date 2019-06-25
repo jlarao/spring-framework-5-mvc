5aa013df9ab1ea303243743894591caa3f6186e8
asusA8\Jorge <pisujo@hotmail.com>
2019-06-24 23:06:52 -0500

    Impl map base service

---
+++ pet-clinic-data/src/main/java/com/jlo/petclinic/services/map/VetSerciceMap.java
@@ -0,0 +1,33 @@
+package com.jlo.petclinic.services.map;
+
+import com.jlo.petclinic.model.Vet;
+import com.jlo.petclinic.services.CrudService;
+
+import java.util.Set;
+
+public class VetSerciceMap  extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
