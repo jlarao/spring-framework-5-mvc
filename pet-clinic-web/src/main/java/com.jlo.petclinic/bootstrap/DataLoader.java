88e5402b64fdc97777aa6dd70df1f280fe2d551a
asusA8\Jorge <pisujo@hotmail.com>
2019-07-22 22:25:37 -0500

    add Pets and Contact info to owners on startup

--- pet-clinic-web/src/main/java/com.jlo.petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com.jlo.petclinic/bootstrap/DataLoader.java
@@ -1,6 +1,7 @@
 package com.jlo.petclinic.bootstrap;
 
 import com.jlo.petclinic.model.Owner;
+import com.jlo.petclinic.model.Pet;
 import com.jlo.petclinic.model.PetType;
 import com.jlo.petclinic.model.Vet;
 import com.jlo.petclinic.services.OwnerService;
@@ -12,6 +13,8 @@ import com.jlo.petclinic.services.map.VetSerciceMap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import sun.tools.jar.CommandLine;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -42,12 +45,33 @@ public class DataLoader implements CommandLineRunner {
        // owner1.setId(1L);
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");
+        owner1.setAddress("direccion 123");
+        owner1.setCity("Miami");
+        owner1.setTelephone("123-4567890");
+
+        Pet mikePet = new Pet();
+        mikePet.setPetType(saveDogPetType);
+        mikePet.setOwner(owner1);
+        mikePet.setBirthDate(LocalDate.now());
+        mikePet.setName("Rosco");
+        owner1.getPets().add(mikePet);
+
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
        // owner2.setId(2L);
         owner2.setFirstName("Fiona");
         owner2.setLastName("Glenanne");
+        owner2.setAddress("direccion 123");
+        owner2.setCity("Miami");
+        owner2.setTelephone("123-4567890");
+
+        Pet fionaCat = new Pet();
+        fionaCat.setPetType(saveCatPetType);
+        fionaCat.setOwner(owner2);
+        fionaCat.setBirthDate(LocalDate.now());
+        fionaCat.setName("gato");
+        owner2.getPets().add(fionaCat);
         ownerService.save(owner2);
 
         System.out.println("Loader Owners ");
