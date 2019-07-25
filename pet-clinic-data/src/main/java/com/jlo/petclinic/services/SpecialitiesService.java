23796f077a44811ba7deb2439bbea6b0bc136546
asusA8\Jorge <pisujo@hotmail.com>
2019-07-25 18:49:01 -0500

    added specialities map impl

---
+++ pet-clinic-data/src/main/java/com/jlo/petclinic/services/SpecialitiesService.java
@@ -0,0 +1,7 @@
+package com.jlo.petclinic.services;
+
+import com.jlo.petclinic.model.BaseEntity;
+import com.jlo.petclinic.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality, Long> {
+}
