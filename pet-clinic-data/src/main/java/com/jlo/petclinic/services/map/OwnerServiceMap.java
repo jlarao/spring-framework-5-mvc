3ef1b1b96ec9fb2b0015b521f4663832757ad8e4
asusA8\Jorge <pisujo@hotmail.com>
2019-07-27 22:23:36 -0500

    fixed broken links

--- pet-clinic-data/src/main/java/com/jlo/petclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/com/jlo/petclinic/services/map/OwnerServiceMap.java
@@ -36,18 +36,19 @@ public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements 
         if(object!=null){
             if(object.getPets()!=null){
                 object.getPets().forEach( pet ->{
-                    if(pet.getPetType()!=null){
-                        if(pet.getPetType().getId()==null){
+                    if(pet.getPetType()!=null) {
+                        if (pet.getPetType().getId() == null) {
                             pet.setPetType(petTypeService.save(pet.getPetType()));
                         }
+                    }
                         else{
                             throw new RuntimeException("PetType is required");
                         }
+
                         if(pet.getId()==null){
                             Pet savedPet = petService.save(pet);
                             pet.setId(savedPet.getId());
                         }
-                    }
                 });
             }
             return super.save(object);
